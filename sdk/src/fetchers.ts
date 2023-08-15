3291b9d9f36a291cdea51defe3b77345fb6596e2
Dodecahedr0x <hexadecifish@gmail.com>
2023-08-15 18:22:24 +0200

    Fetchers

---
+++ sdk/src/fetchers.ts
@@ -0,0 +1,61 @@
+import { Program, Provider } from "@coral-xyz/anchor";
+import { NftStandard } from "./idl/nft_standard";
+import IDL from "./idl/nft_standard.json";
+import { NFT_STANDARD_PROGRAM_ID } from "./constants";
+import { PublicKey } from "@solana/web3.js";
+import {
+  getAuthoritiesGroupKey,
+  getInclusionKey,
+  getMetadataKey,
+  getSupersetInclusionKey,
+} from "./pdas";
+
+const getProgram = (provider: Provider) => {
+  return new Program<NftStandard>(
+    IDL as any,
+    NFT_STANDARD_PROGRAM_ID,
+    provider
+  );
+};
+
+export const fetchAuthoritiesGroupById = async (
+  provider: Provider,
+  id: PublicKey
+) => {
+  return await getProgram(provider).account.authoritiesGroup.fetch(
+    getAuthoritiesGroupKey(id)
+  );
+};
+
+export const fetchAuthoritiesGroupByKey = async (
+  provider: Provider,
+  key: PublicKey
+) => {
+  return await getProgram(provider).account.authoritiesGroup.fetch(key);
+};
+
+export const fetchMetadata = async (provider: Provider, mint: PublicKey) => {
+  return await getProgram(provider).account.metadata.fetch(
+    getMetadataKey(mint)
+  );
+};
+
+export const fetchInclusion = async (
+  provider: Provider,
+  parentMint: PublicKey,
+  childMint: PublicKey
+) => {
+  return await getProgram(provider).account.inclusion.fetch(
+    getInclusionKey(parentMint, childMint)
+  );
+};
+
+export const fetchSupersetInclusion = async (
+  provider: Provider,
+  parentMint: PublicKey,
+  childMint: PublicKey
+) => {
+  return await getProgram(provider).account.inclusion.fetch(
+    getSupersetInclusionKey(parentMint, childMint)
+  );
+};
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -5,3 +5,4 @@ export * from "./metadataData";
 export * from "./superset";
 export * from "./utils";
 export * from "./builders";
+export * from "./fetchers";
