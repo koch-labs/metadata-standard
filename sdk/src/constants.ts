505ed853905095954a2942c58074e8b14695789b
Dodecahedr0x <hexadecifish@gmail.com>
2023-09-18 11:05:13 +0200

    Bump to 0.0.13

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -3,12 +3,6 @@ import { PublicKey } from "@solana/web3.js";
 export const METADATA_STANDARD_PROGRAM_ID = new PublicKey(
   "9msweUGitRR1ELUe4XZi6xhecPCko54kSqSnfWH7LLiZ"
 );
-export const SHADOW_NFT_PROGRAM_ID = new PublicKey(
-  "9fQse1hBRfzWweeUod6WEsR4jZf7hVucetEheCaWooY5"
-);
-export const RENT_NFT_PROGRAM_ID = new PublicKey(
-  "FQ9MA87E8H8B3aGxnGQwmXNGzgRTap9WL9Yhk3GZT9w8"
-);
 export const AUTHORITIES_SEED = "authorities";
 export const METADATA_SEED = "metadata";
 export const INCLUSION_SEED = "inclusion";
